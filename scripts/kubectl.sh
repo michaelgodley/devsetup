f54706c50da8e24ae7ade05d3f26b8f59b5ab5bd
Mike Godley <michaelgodley@gmail.com>
2023-07-03 17:48:48 +0100

    Updated Rancher and kubectl scripts

--- scripts/kubectl.sh
+++ scripts/kubectl.sh
@@ -3,7 +3,17 @@
 . ../config.conf
 . ./libs.sh
 
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubectl
+mkdir ~/.kube
+chmod -R go-rx ~/.kube
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
+rm kubectl
+
+## Using packagement management
+# sudo apt-get install -y ca-certificates curl
+# curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+# echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+# sudo apt-get update
+# sudo apt-get install -y kubectl
--- scripts/rancher.sh
+++ scripts/rancher.sh
@@ -3,6 +3,4 @@
 . ../config.conf
 . libs.sh
 
-docker run -d --restart=unless-stopped -p 80:80 -p 443:443 --privileged rancher/rancher:$RANCHER_VERSION
-mkdir ~/.kube
-chmod -R go-rx ~/.kube
+docker run -d --name rancher --restart=unless-stopped -p 80:80 -p 443:443 --privileged rancher/rancher:$RANCHER_VERSION
